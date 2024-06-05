bd7c17bd0760d3d72f73c4bb70ad38dcdae357d2
Shuntia <128505810+shuntia@users.noreply.github.com>
2024-06-05 10:15:49 -0700

    tested file io

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use std::fs::File;
+use std::io::prelude::*;
+use std::path::Path;
+
 fn main() {
-    println!("Hello, world!");
+    let path = Path::new("C:/Users/shunt/playground/wineglass/interpret/main.wg");
+    let display=path.display();
+    let mut file = match File::open(&path) {
+        Err(why) => panic!("couldn't open {}: {}", display, why),
+        Ok(file) => file,
+    };
+    let mut s = String::new();
+    match file.read_to_string(&mut s) {
+        Err(why) => panic!("couldn't read {}: {}", display, why),
+        Ok(_) => println!("{} contains:\n{}", display, s),
+    }
 }
