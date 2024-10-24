bc5ea95a4952b85b487b7b00508ca85b4be05665
Shuntia <128505810+shuntia@users.noreply.github.com>
2024-10-24 11:31:56 -0700

    refactoring

---
+++ bottle/src/lib.rs
@@ -0,0 +1,95 @@
+pub mod lexer;
+pub mod parser;
+pub mod err;
+use configmgr::config;
+use log::*;
+use std::path::{Path, PathBuf};
+
+pub fn start(path: &String) -> Result<i32, String> {
+    if !Path::new(path).exists() {
+        panic!("File does not exist: {}", path);
+    }
+    info!("File found.");
+    Ok(0)
+}
+
+pub enum State {
+    Standard(i32),
+    Error(i32),
+    Critical(i32),
+    Paused(i32),
+    Defective(i32),
+    Fatal(i64),
+    Shattered(i64),
+}
+pub struct Bottle {
+    pub state: State,
+    pub name: String,
+    pub path: PathBuf,
+    pub version: config::Version,
+    pub description: String,
+    pub lexer: lexer::Lexer,
+}
+
+impl Bottle {
+    pub fn new<P: AsRef<Path>>(
+        targetpath: P,
+        name: Option<&str>,
+        ver: Option<config::Version>,
+        description: Option<&str>,
+    ) -> Result<Bottle, String> {
+        let path = targetpath.as_ref().to_path_buf();
+        if !path.exists() {
+            return Err(format!("File does not exist: {}", path.display()));
+        }
+        info!("Target found: {}", path.display());
+        let file = match std::fs::File::open(&path) {
+            Ok(f) => f,
+            Err(e) => return Err(format!("Failed to open file: {}", e)),
+        };
+        let lexer = match lexer::Lexer::from_path(&targetpath) {
+            Ok(l) => l,
+            Err(e) => return Err(format!("Failed to create lexer: {}", e)),
+        };
+        return Ok(Bottle {
+            state: State::Standard(0),
+            name: match name {
+                Some(n) => n.to_string(),
+                None => path.file_name().unwrap().to_str().unwrap().to_string(),
+            },
+            path,
+            version: match ver {
+                Some(v) => v,
+                None => config::Version::get_crate_ver(),
+            },
+            description: match description {
+                Some(d) => d.to_string(),
+                None => String::new(),
+            },
+            lexer,
+        });
+    }
+    pub fn start(&mut self) {
+        println!("Starting bottle: {}", self.name);
+        println!("Version: {}", self.version);
+        println!("Description: {}", self.description);
+        println!("Path: {}", self.path.display());
+        println!("lexing...");
+        self.lexer.by_ref().for_each(|x| match x {
+            Ok(l) => println!("{:?}", l),
+            Err(e) => eprintln!("Error: {}", e),
+        });
+    }
+}
+
+//for errors
+impl Bottle {
+    pub fn error(&mut self, e: crate::err::Error) {
+        self.state = State::Error(e.code);
+        error!("Error in bottle {}: {}", self.name, e.message)
+    }
+    pub fn shatter(&mut self, report: crate::err::InternalReport) {
+        self.state = State::Shattered(report.get_code());
+        error!("Bottle shattered {}: {}", self.name, report.get_message());
+    }
+}
