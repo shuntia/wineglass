50605f0761e7f681ca184585cd7ca27998334b7b
Shuntia <128505810+shuntia@users.noreply.github.com>
2024-11-07 16:49:28 -0800

    updated workflow

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,6 @@ use std::path::MAIN_SEPARATOR_STR;
 fn main() {
     env::set_var("RUST_BACKTRACE", "1");
     env_logger::init();
-    let args = env::args().collect::<Vec<String>>();
     let target: Vec<String> = vec![
         env::current_dir().unwrap().to_str().unwrap().to_string(),
         "interpret\\main.wg".to_string(),
@@ -16,7 +15,6 @@ fn main() {
     let sep: &str = MAIN_SEPARATOR_STR;
     let target_path_str = target.join(sep);
     let target_path = Path::new(&target_path_str);
-    let com_args = configmgr::config::Args::new();
     let mut file: File;
     match target_path.exists() {
         true => {
@@ -25,11 +23,7 @@ fn main() {
                 Ok(file) => file,
             };
         }
-        false => panic!("FATAL! Target file not found: {}", target_path.display()),
-        _ => panic!(
-            "FATAL! Unknown error! failed to check if target path exists!: {}",
-            target_path.display()
-        ),
+        false => panic!("FATAL! Target file not found: {}", target_path.display())
     };
     let mut contents = String::new();
     match file.read_to_string(&mut contents) {
